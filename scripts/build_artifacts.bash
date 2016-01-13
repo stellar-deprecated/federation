3b922c78f5cb4354cbc7c64f8dfed88065ca0ccd
Bartek Nowotarski <bartek@nowotarski.info>
2016-01-13 12:45:28 +0100

    Fix build_artifacts.bash
    gb maintainers merged constabulary/gb#533 and now all cross-compiled
    binaries' names have the same format.

--- scripts/build_artifacts.bash
+++ scripts/build_artifacts.bash
@@ -30,11 +30,7 @@ build() {
 
 srcBin() {
   GOOS=$1
-  BIN="federation"
-
-  if [ "$GOOS" != "$CURRENT_GOOS" ]; then
-    BIN+="-$GOOS-$GOARCH"
-  fi
+  BIN="federation-$GOOS-$GOARCH"
 
   if [ "$GOOS" = "windows" ]; then
     BIN+=".exe"
